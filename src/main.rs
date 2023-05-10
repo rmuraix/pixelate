4ee782dc7d7b2e2b5bc2ac165336d2747fdd1720
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-05-10 12:29:09 +0900

    Add temporary parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+    /// Path to the image file to be processed
+    #[arg(short, long, value_name = "FILE")]
+    target: PathBuf,
+
+    /// Output path of the processed image file.
+    #[arg(short, long, value_name = "FILE")]
+    out: PathBuf,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// test values
+        #[arg(short, long)]
+        list: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Test { list } => {
+            println!("{}", list)
+        }
+    }
 }
