0761e1f676b8da6406e298ea3e35fa2c45d24ed4
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-05-25 14:40:22 +0900

    Halftoning supports color images

--- src/filters/binarization.rs
+++ src/filters/binarization.rs
@@ -1,29 +1,26 @@
 use std::path::PathBuf;
 
-use image::{ImageBuffer, Luma};
+use image::{ImageBuffer, Rgb, RgbImage};
 
-use super::grayscale;
-
-pub fn halftoning(path: PathBuf) -> ImageBuffer<Luma<u8>, Vec<u8>> {
-    let mut gray_img: ImageBuffer<Luma<u8>, Vec<u8>> = grayscale(path, 0.2126, 0.7152, 0.0722);
-    let size_x: u32 = gray_img.width();
-    let size_y: u32 = gray_img.height();
+pub fn halftoning(path: PathBuf) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let img: image::DynamicImage = image::open(path).unwrap();
+    let img = img.to_rgb8();
+    let (width, height) = img.dimensions();
     // dither method
     // Bayar pattern
     let pattern: [[u32; 4]; 4] = [[0, 8, 2, 10], [12, 4, 14, 6], [3, 11, 1, 9], [15, 7, 13, 5]];
-    for y in 0..size_y {
-        for x in 0..size_x {
-            let pixel: &Luma<u8> = gray_img.get_pixel(x, y);
-            let threshold: u32 = (pattern[(x % 4) as usize][(y % 4) as usize]) * 16 + 8;
-            if pixel[0] as u32 >= threshold {
-                let val = [255 as u8; 1];
-                gray_img.put_pixel(x, y, image::Luma(val));
+    let mut imgbuf: RgbImage = ImageBuffer::new(width, height);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let threshold: u32 = (pattern[(x % 4) as usize][(y % 4) as usize]) * 16 + 8;
+        let img_pixel = img.get_pixel(x, y);
+        for n in 0..3 {
+            if img_pixel[n] as u32 >= threshold {
+                pixel[n] = 255;
             } else {
-                let val = [0 as u8; 1];
-                // Writing pixel data
-                gray_img.put_pixel(x, y, image::Luma(val));
+                pixel[n] = 0;
             }
         }
     }
-    return gray_img;
+    return imgbuf;
 }
