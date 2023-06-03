dcb101a06bbb1a3e83a6510998ea4cdadbf989dd
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-06-03 16:40:12 +0900

    Remove return

--- src/filters.rs
+++ src/filters.rs
@@ -7,10 +7,10 @@ mod gamma;
 mod grayscale;
 
 pub fn grayscale(path: PathBuf, red: f64, green: f64, blue: f64) -> ImageBuffer<Luma<u8>, Vec<u8>> {
-    return grayscale::grayscale(path, red, green, blue);
+    grayscale::grayscale(path, red, green, blue)
 }
 pub fn halftoning(path: PathBuf) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-    return binarization::halftoning(path);
+    binarization::halftoning(path)
 }
 pub fn gamma(path: PathBuf, gamma: f64) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     gamma::main(path, gamma)
